837496e9e76e1dbd17cf3a9ef8f58250f33b9a17
Przemyslaw Goraj <przemyslaw.goraj@expert4you.pl>
2025-02-19 07:39:03 +0100

    test

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function getNextQuote() {
 	return `Prawo ${citation.law} - ${citation.quote}\n${citation.explanation}`;
 }
 
-cron.schedule('30 6 * * *', () => {
+cron.schedule('45 7 * * *', () => {
 	const dailyQuote = getNextQuote();
 	bot
 		.sendMessage(process.env.CHAT_ID, dailyQuote)
